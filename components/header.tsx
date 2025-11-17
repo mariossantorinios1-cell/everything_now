502858db8d3aecee92b2ed5b983742b51fca1896
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-17 10:26:16 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,7 @@
-// components/header.tsx
 export default function Header() {
   return (
-    <header className="w-full p-6 bg-black text-white">
-      <div className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Everything Now</h1>
-        <nav>
-          <a href="#" className="px-4">Home</a>
-          <a href="#" className="px-4">Explore</a>
-          <a href="#" className="px-4">Trending</a>
-          <a href="#" className="px-4">Live</a>
-        </nav>
-      </div>
+    <header className="w-full p-4 bg-black text-white text-xl font-bold">
+      EVERYTHING NOW
     </header>
   );
 }
