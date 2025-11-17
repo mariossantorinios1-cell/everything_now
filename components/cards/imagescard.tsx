672085346446b0a421a65c71a2de9a2f9c2ebff7
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-17 10:34:08 +0200

    Delete components/cards directory

--- components/cards/imagescard.tsx
+++
@@ -1,8 +0,0 @@
-export default function ImagesCard() {
-  return (
-    <div className="p-4 border rounded-lg shadow">
-      <h2 className="text-lg font-bold">Images Card</h2>
-      <p>Coming soon...</p>
-    </div>
-  );
-}
