69a9911740e78d65a0b02c3c85538b433bb6bde1
mariossantorinios1-cell <marios.santorinios1@gmail.com>
2025-11-17 10:50:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
-import ImagesCard from "../components/cards/imagescard";
+import ImagesCard from "../components/cards/ImagesCard";
+import VideoCard from "../components/cards/VideoCard";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="p-6">
       <Header />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+        <VideoCard />
         <ImagesCard />
       </div>
     </main>
